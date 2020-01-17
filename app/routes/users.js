f06ae20b284b23f43ec5a76e3f89fe6750186663
abhinav098 <abhinav.g@kiprosh.com>
2020-01-17 19:01:27 +0530

    Overview Done

---
+++ app/routes/users.js
@@ -0,0 +1,17 @@
+import Route from '@ember/routing/route';
+
+export default class UsersRoute extends Route {
+  model() {
+    return [{
+      name: "Brent Schooley",
+      twitter: "@brentschooley"
+    }, {
+      name: "Sam Agnew",
+      twitter: "@sagnewshreds"
+    }, {
+      name: "Eddie Zaneski",
+      twitter: "@eddiezane"
+    }];
+  }
+
+}
