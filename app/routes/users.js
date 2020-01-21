41a728d627c0df79cb4fd98b2b74bf2d827f6005
Shrey-tech <shreyash@kiprosh.com>
2020-01-21 12:54:40 +0530

    username changed successfully

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,17 +1,20 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default class UsersRoute extends Route {
   model() {
-    return [{
-      name: "Brent Schooley",
-      twitter: "@brentschooley"
-    }, {
-      name: "Sam Agnew",
-      twitter: "@sagnewshreds"
-    }, {
-      name: "Eddie Zaneski",
-      twitter: "@eddiezane"
-    }];
+    return [
+      {
+        name: "Brent Schooley",
+        twitter: "@brentschooley"
+      },
+      {
+        name: "Sam Agnew",
+        twitter: "@sagnewshreds"
+      },
+      {
+        name: "Eddie Zaneski",
+        twitter: "@eddiezane"
+      }
+    ];
   }
-
 }
